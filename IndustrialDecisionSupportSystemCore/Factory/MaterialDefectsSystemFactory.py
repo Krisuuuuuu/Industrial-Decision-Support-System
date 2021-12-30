1bb463a27350051ace78baf1a3c74493b3bc2e1d
Krisuuuuuu <krisu1997@onet.pl>
2021-12-30 15:10:50 +0100

    Added factory for material defects inference system

---
+++ IndustrialDecisionSupportSystemCore/Factory/MaterialDefectsSystemFactory.py
@@ -0,0 +1,13 @@
+import IndustrialDecisionSupportSystemCore.Factory.Base.AbstractFactory as af
+import IndustrialDecisionSupportSystemCore.Builder.MaterialDefectsSystemBuilder as b
+
+
+class MaterialDefectsSystemFactory(af.AbstractFactory):
+    def __init__(self):
+        super().__init__()
+
+    def create(self):
+        self._director.set_builder(b.MaterialDefectsInferenceSystemBuilder())
+        system_instance = self._director.get_system_instance()
+
+        return  system_instance
