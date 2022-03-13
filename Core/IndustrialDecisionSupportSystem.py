51d24de7b35d69bbe58cbbe15d976cebed61bc16
Krisuuuuuu <krisu1997@onet.pl>
2022-03-13 17:36:07 +0100

    Perform clean up action

--- Core/IndustrialDecisionSupportSystem.py
+++ Core/IndustrialDecisionSupportSystem.py
@@ -1,17 +1,10 @@
-import Core.Factory.ChemicalCompositionSystemFactory as cs
-
 
 class IndustrialDecisionSupportSystem:
     def __init__(self):
         self._inference_system = None
 
     def start_evaluation(self):
-        self._evaluate_chemical_composition()
-
-    def _evaluate_chemical_composition(self):
-        self._dispose_inference_system()
-        self._prepare_inference_system(cs.ChemicalCompositionSystemFactory())
-        self._inference_system.evaluate_results()
+        print("test")
 
     def _prepare_inference_system(self, abstract_factory):
         self._inference_system = abstract_factory.create()
